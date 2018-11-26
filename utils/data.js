0a11b87b7721d948ce13c17a88b934780964bfb9
zzq <Keen_Team@163.com>
2018-11-26 09:09:39 +0800

    wexin

--- utils/data.js
+++ utils/data.js
@@ -85,7 +85,7 @@ function getIndexNavSectionData() {
 
         subject: "商城小程序开发",
         civerpath: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1581514102,2252347240&fm=26&gp=0.jpg",
-        price: "￥10000+",
+        price: "￥10000",
         message: '先的企业级云服务商,致力于打造以数据为核心的场景化 PaaS服务'
 
       },
@@ -94,7 +94,7 @@ function getIndexNavSectionData() {
 
         subject: "企业官网小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2378687199,1771440802&fm=11&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '的PaaS云服务.七牛存储 七牛云为企业提供对象存储服'
 
       },
@@ -103,7 +103,7 @@ function getIndexNavSectionData() {
 
         subject: "附近门店小程序开发",
         civerpath: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1227504765,1936071277&fm=26&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '伊本造型是由著名形象设计师杨进先生创办。'
 
       },
@@ -111,7 +111,7 @@ function getIndexNavSectionData() {
 
         subject: "PC网站+微信公共号+小程序",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1510221398,283604977&fm=26&gp=0.jpg",
-        price: "￥15000+",
+        price: "￥15000",
         message: '我们最求得是没有最长的时间特价'
 
       },
@@ -120,7 +120,7 @@ function getIndexNavSectionData() {
 
         subject: "附近旅游类小程序开发",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1416663764,810730327&fm=26&gp=0.jpg",
-        price: "￥1000+",
+        price: "￥1000",
         message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
 
       }
@@ -131,7 +131,7 @@ function getIndexNavSectionData() {
 
         subject: "商城小程序开发",
         civerpath: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1581514102,2252347240&fm=26&gp=0.jpg",
-        price: "￥10000+",
+        price: "￥10000",
         message: '先的企业级云服务商,致力于打造以数据为核心的场景化 PaaS服务'
 
       },
@@ -140,7 +140,7 @@ function getIndexNavSectionData() {
 
         subject: "企业官网小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2378687199,1771440802&fm=11&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '的PaaS云服务.七牛存储 七牛云为企业提供对象存储服'
 
       },
@@ -149,7 +149,7 @@ function getIndexNavSectionData() {
 
         subject: "附近门店小程序开发",
         civerpath: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1227504765,1936071277&fm=26&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '伊本造型是由著名形象设计师杨进先生创办。'
 
       },
@@ -157,7 +157,7 @@ function getIndexNavSectionData() {
 
         subject: "PC网站+微信公共号+小程序",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1510221398,283604977&fm=26&gp=0.jpg",
-        price: "￥15000+",
+        price: "￥15000",
         message: '我们最求得是没有最长的时间特价'
 
       },
@@ -166,7 +166,7 @@ function getIndexNavSectionData() {
 
         subject: "附近旅游类小程序开发",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1416663764,810730327&fm=26&gp=0.jpg",
-        price: "￥1000+",
+        price: "￥1000",
         message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
 
       }
@@ -177,7 +177,7 @@ function getIndexNavSectionData() {
 
         subject: "商城小程序开发",
         civerpath: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1581514102,2252347240&fm=26&gp=0.jpg",
-        price: "￥10000+",
+        price: "￥10000",
         message: '先的企业级云服务商,致力于打造以数据为核心的场景化 PaaS服务'
 
       },
@@ -186,7 +186,7 @@ function getIndexNavSectionData() {
 
         subject: "企业官网小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2378687199,1771440802&fm=11&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '的PaaS云服务.七牛存储 七牛云为企业提供对象存储服'
 
       },
@@ -195,7 +195,7 @@ function getIndexNavSectionData() {
 
         subject: "附近门店小程序开发",
         civerpath: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1227504765,1936071277&fm=26&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '伊本造型是由著名形象设计师杨进先生创办。'
 
       },
@@ -203,7 +203,7 @@ function getIndexNavSectionData() {
 
         subject: "PC网站+微信公共号+小程序",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1510221398,283604977&fm=26&gp=0.jpg",
-        price: "￥15000+",
+        price: "￥15000",
         message: '我们最求得是没有最长的时间特价'
 
       },
@@ -212,7 +212,7 @@ function getIndexNavSectionData() {
 
         subject: "附近旅游类小程序开发",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1416663764,810730327&fm=26&gp=0.jpg",
-        price: "￥1000+",
+        price: "￥1000",
         message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
 
       }
@@ -223,7 +223,7 @@ function getIndexNavSectionData() {
 
         subject: "商城小程序开发",
         civerpath: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1581514102,2252347240&fm=26&gp=0.jpg",
-        price: "￥10000+",
+        price: "￥10000",
         message: '先的企业级云服务商,致力于打造以数据为核心的场景化 PaaS服务'
 
       },
@@ -232,7 +232,7 @@ function getIndexNavSectionData() {
 
         subject: "企业官网小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2378687199,1771440802&fm=11&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '的PaaS云服务.七牛存储 七牛云为企业提供对象存储服'
 
       },
@@ -241,7 +241,7 @@ function getIndexNavSectionData() {
 
         subject: "附近门店小程序开发",
         civerpath: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1227504765,1936071277&fm=26&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '伊本造型是由著名形象设计师杨进先生创办。'
 
       },
@@ -249,7 +249,7 @@ function getIndexNavSectionData() {
 
         subject: "PC网站+微信公共号+小程序",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1510221398,283604977&fm=26&gp=0.jpg",
-        price: "￥15000+",
+        price: "￥15000",
         message: '我们最求得是没有最长的时间特价'
 
       },
@@ -258,7 +258,7 @@ function getIndexNavSectionData() {
 
         subject: "附近旅游类小程序开发",
         civerpath: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1416663764,810730327&fm=26&gp=0.jpg",
-        price: "￥1000+",
+        price: "￥1000",
         message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
 
       }
@@ -268,7 +268,7 @@ function getIndexNavSectionData() {
 
         subject: "商城小程序开发",
         civerpath: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1581514102,2252347240&fm=26&gp=0.jpg",
-        price: "￥10000+",
+        price: "￥10000",
         message: '先的企业级云服务商,致力于打造以数据为核心的场景化 PaaS服务'
 
       },
@@ -277,7 +277,7 @@ function getIndexNavSectionData() {
 
         subject: "企业官网小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2378687199,1771440802&fm=11&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '的PaaS云服务.七牛存储 七牛云为企业提供对象存储服'
 
       },
@@ -286,7 +286,7 @@ function getIndexNavSectionData() {
 
         subject: "附近门店小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=786771431,2652373896&fm=26&gp=0.jpg",
-        price: "￥5000+",
+        price: "￥5000",
         message: '伊本造型是由著名形象设计师杨进先生创办。'
 
       },
@@ -294,7 +294,7 @@ function getIndexNavSectionData() {
 
         subject: "PC网站+微信公共号+小程序",
         civerpath: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=786771431,2652373896&fm=26&gp=0.jpg",
-        price: "￥15000+",
+        price: "￥15000",
         message: '我们最求得是没有最长的时间特价'
 
       },
@@ -303,7 +303,7 @@ function getIndexNavSectionData() {
 
         subject: "附近旅游类小程序开发",
         civerpath: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=786771431,2652373896&fm=26&gp=0.jpg",
-        price: "￥1000+",
+        price: "￥1000",
         message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
 
       }
